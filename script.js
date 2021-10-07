7bd52c5369063793e666c624db1c40a69f67fe88
JQ <jcq5010@gmail.com>
2021-10-07 13:24:09 -0400

    revised API URL and added my notes labeled as JQ

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ var flkrList = document.querySelector('ul');
 var fetchButton = document.getElementById('fetch-button');
 
 function getApi() {
-    var requestUrl = 'https://api.githhttps://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff186f827916d3c42a8a2f504e5903d4&tags=get+recent&extras=&per_page=6&page=&format=json&nojsoncallback=1ub.com/users/amaddatu/repos';
+    // Looks like you got 99% of the way there!  I think you just had a few mistaken copy and pastes in the URL below.  Check out how I revised below 
+    // var requestUrl = 'https://api.githhttps://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff186f827916d3c42a8a2f504e5903d4&tags=get+recent&extras=&per_page=6&page=&format=json&nojsoncallback=1ub.com/users/amaddatu/repos';
+
+    var testKeyword = "ocean"
+    var requestUrl = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff186f827916d3c42a8a2f504e5903d4&tags=${testKeyword}&format=json&nojsoncallback=1`;
 
     fetch(requestUrl)
         .then(function (response) {
@@ -13,16 +17,26 @@ function getApi() {
         .then(function (data) {
             console.log(data);
 
-            for (var i = 0; i < data.length; i++) {
+            // Once you get the data console logged above you will see that the actual photos array can be accessed by the property ".photos" followed by ".photo" (strange that they named the array of photos "photo"!)
+            var photos = data.photos.photo
+
+            // Loop over photos (I am only pulling 8 here so we don't loop through and render all results)
+            for (var i = 0; i < 8; i++) {
                 var listItem = document.createElement('li');
 
-                listItem.textContent = data[i].html_url;
+                // I believe you were setting textContent to .html_url, however, that property does not exist on these photo objects.  Console.log the photos array and check out the properties on each photo (pulling just title for now to get something on the screen).  To actually build an image it looks like flickr uses a URL string that includes "server", "id", and "secret", all which are properties on each photo object.  I found this in the flickr documentation under URLs:
+
+                // https://live.staticflickr.com/{server-id}/{id}_{secret}.jpg
+
+                listItem.textContent = photos[i].title
                 flkrList.appendChild(listItem);
             }
         });
 }
 
 fetchButton.addEventListener('click', getApi);
+
+
 
 // Hero
 // Key:
